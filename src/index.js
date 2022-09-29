8a8aca0f5cb0e1581810f5c25f197a41d7825c71
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-29 12:11:01 +0200

    webpackconfig installed

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles.css';
+
+function reg() {
+    console.log('Müködik');
+}
+
+document.getElementById('gomb').addEventListener('click',reg);
