7b1d49e846fc1995d9e34680b6e44f0a067e1bb9
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-29 12:55:33 +0200

    password min 8 jó

--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './styles.css';
 
 function reg() {
-    let hossz = document.getElementById('username').value.toString();
+    let hossz = document.getElementById('username').value;
+    let jelszo1 = document.getElementById('password1').value;
+    let jelszo2 = document.getElementById('password2').value;
+
     if ( hossz.length >= 3) {        
         document.getElementById('username').value.toLowerCase();
     } else{
         window.alert('Nem elég hosszú!');
     }
     if (document.getElementById('beemail').value.includes('@')) {
-        console.log('Müködik a @');
+    }else{
+        window.alert('Az email nem tartalmaz @-ot.');
+    }    
+    if (jelszo1.length >= 8 && jelszo2.length >= 8) {
+        console.log('Megvan a minimum 8 karakter');
+    }else{
+        window.alert('Valamelyik jelszó nem elég hosszú.');
     }
-    
     
 
 }
